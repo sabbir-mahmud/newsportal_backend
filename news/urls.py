26bb9d53ea4111d3fa087570823a1c088a4493f7
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2025-05-21 12:27:24 +0600

    Init setup with auth api

---
+++ news/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("auth/", include("apps.authentication.urls")),
+]
